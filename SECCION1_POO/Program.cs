56075d77d6cf31efcab56c2e4bd0db4f6411642c
Elmer-Fuentes <delacruzelmer27@gmail.com>
2026-07-16 19:04:30 -0600

    Agregar archivos de proyecto.

---
+++ SECCION1_POO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SECCION1_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
